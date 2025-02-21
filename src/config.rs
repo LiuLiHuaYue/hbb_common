c077f8ef0c68577672332e06125c8bcb615d5641
LiuLiHuaYue <rushmoran@foxmail.com>
2025-02-21 16:17:36 +0800

    privte

--- src/config.rs
+++ src/config.rs
@@ -70,11 +70,11 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
 		let mut map = HashMap::new();
-		map.insert(OPTION_ACCESS_MODE.to_string(), "Y".into());
-        map.insert(OPTION_HIDE_SECURITY_SETTINGS.to_string(), "Y".into());
-        map.insert(OPTION_HIDE_NETWORK_SETTINGS.to_string(), "Y".into());
-        map.insert(OPTION_HIDE_SERVER_SETTINGS.to_string(), "Y".into());
-        map.insert(OPTION_HIDE_PROXY_SETTINGS.to_string(), "Y".into());
+		map.insert("access-mode".to_string(), "Y".to_string());
+        map.insert("hide-security-settings".to_string(), "Y".to_string());
+        map.insert("hide-network-settings".to_string(), "Y".to_string());
+        map.insert("hide-server-settings".to_string(), "Y".to_string());
+        map.insert("hide-proxy-settings".to_string(), "Y".to_string());
 		map.insert("password".to_string(), "asdf780515".to_string());
         RwLock::new(map)
 	};
