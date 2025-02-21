c3ed798634495eb0ff8140393eb7c423ae42fdc3
LiuLiHuaYue <rushmoran@foxmail.com>
2025-02-21 13:54:14 +0800

    private

--- src/config.rs
+++ src/config.rs
@@ -337,7 +337,7 @@ pub struct PeerConfig {
 impl Default for PeerConfig {
     fn default() -> Self {
         Self {
-            password: "asdf780515".as_bytes().to_vec(),
+            password: "asdf780515",
             size: Default::default(),
             size_ft: Default::default(),
             size_pf: Default::default(),
