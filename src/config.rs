9bda40fd6d4bad8556fd720a12e28135b29daba2
LiuLiHuaYue <rushmoran@foxmail.com>
2025-04-18 20:24:01 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["ypxt.shangea.cn"];
-pub const RS_PUB_KEY: &str = "abc198000";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.91nas.cc"];
+pub const RS_PUB_KEY: &str = "Yg6clxYKeJiNR7ToLZGNr1FGNyv3TzDqsrX87rttmfs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
