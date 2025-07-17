fcb276366915f4cdb5776f0d790b6b61dbe78d4e
LiuLiHuaYue <rushmoran@foxmail.com>
2025-07-17 23:12:33 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -104,7 +104,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["8.130.149.25"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["8.130.93.236"];
 pub const RS_PUB_KEY: &str = "ASDasd123456";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
