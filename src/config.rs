cf37727f1c1a1b81073c5d6e49106735ffd10314
LiuLiHuaYue <rushmoran@foxmail.com>
2025-09-04 17:43:44 +0800

    update

--- src/config.rs
+++ src/config.rs
@@ -63,7 +63,12 @@ lazy_static::lazy_static! {
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
     pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = {
+                                                                        let mut map = HashMap::new();
+                                                                        map.insert("lang".into(), "zh_cn".into()); // 完全控制
+                                                                        map.insert("theme".into(), "dark".into()); // IP直连
+                                                                        RwLock::new(map)
+                                                                       };
     pub static ref DEFAULT_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
