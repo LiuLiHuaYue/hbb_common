607b7238cdfe6fac161c2fc896ddcb6113ec9930
LiuLiHuaYue <rushmoran@foxmail.com>
2025-11-04 10:29:21 +0800

    up

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.qkx888.top"];
+pub const RS_PUB_KEY: &str = "M3y2qOPREAEt6DlXABm5havvhnqZAKdQiJ1Tc57YT0c=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
