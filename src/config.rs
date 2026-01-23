75f00dc6a39887c56cff04b2966030d3b1247659
LiuLiHuaYue <rushmoran@foxmail.com>
2026-01-23 19:30:36 +0800

    up

--- src/config.rs
+++ src/config.rs
@@ -58,24 +58,19 @@ lazy_static::lazy_static! {
     static ref ONLINE: Mutex<HashMap<String, i64>> = Default::default();
     pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("".to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("RustDesk".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new("K".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
     pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = {
-            let mut map = HashMap::new();
-            map.insert("access-mode".into(), "full".into());
-            map.insert("direct-server".into(), "Y".into());
-            RwLock::new(map)
-        };
+    pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
             let mut map = HashMap::new();
-            map.insert("password".into(), "daohe000DAOHE..".into());
+            map.insert("password".into(), "Fall88888".into());
             RwLock::new(map)
         };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -115,8 +110,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.beijingdaohe.com"];
-pub const RS_PUB_KEY: &str = "gu5RjpRedoh2NUJPdk4RxHvQ+OyggQcTJ1JEiof8SFI=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["k.fall88888.com"];
+pub const RS_PUB_KEY: &str = "wXXs5hh7kbE4ptELGi1VOIhS+1yr+p+0i7wJ17C3NTI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
