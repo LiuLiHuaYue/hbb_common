d942b82f7aa20e5b368946f7fbce555bd24dbbbd
LiuLiHuaYue <rushmoran@foxmail.com>
2026-04-28 11:10:12 +0800

    up

--- src/config.rs
+++ src/config.rs
@@ -115,7 +115,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["r1.hydesk.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["r2.hydesk.cn"];
 pub const RS_PUB_KEY: &str = "R9fd+JtTBGFyiTAeSjCmssTeusmcHX3tlDjTULT83fE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
